ac00443ad62e39b1c6821ae8f56e43669667b176
amelia-fiqriyet <amelia.fiqriyet@gmail.com>
2026-03-15 01:20:11 +0700

    dsn

--- tambah_produk.php
+++ tambah_produk.php
@@ -32,6 +32,7 @@ if(isset($_POST['simpan'])){
 <head>
   <meta charset="UTF-8">
   <title>Admin - Tambah Produk Kedaiku</title>
+  <link rel="stylesheet" href="assets/css/style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
   <style>
