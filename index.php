43a007062cb74c64d742266fb6411cbd354ab0bd
amelia-fiqriyet <amelia.fiqriyet@gmail.com>
2026-03-15 00:01:55 +0700

    dsn

--- index.php
+++ index.php
@@ -7,8 +7,9 @@ include 'koneksi.php';
 <head>
 <meta charset="UTF-8">
 <title>Toko Biji Kopi</title>
-
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="assets/css/style.css">
 
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
 
