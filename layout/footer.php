c5bd6949c8684497270d3ec71b68c6a9bb24ed1a
amelia-fiqriyet <amelia.fiqriyet@gmail.com>
2026-03-14 23:48:32 +0700

    dsn

--- layout/footer.php
+++ layout/footer.php
@@ -1,8 +1,5 @@
-<footer class="text-white text-center py-3 mt-5">
+<footer class="text-white text-center py-3">
+
 <p>&copy; <?php echo date("Y"); ?> Toko Biji Kopi. All rights reserved.</p>
+
 </footer>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
-</body>
-</html>
