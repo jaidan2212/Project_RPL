d47195ec01bdbab451857e4f980505bdfa840731
amelia-fiqriyet <amelia.fiqriyet@gmail.com>
2026-03-15 00:10:51 +0700

    dsn

--- login.php
+++ login.php
@@ -38,7 +38,7 @@ if(isset($_POST['login'])){
 <html lang="id">
 <head>
 <meta charset="UTF-8">
-<title>Login - Kopiku</title>
+<title>Login - Kedaiku</title>
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 
